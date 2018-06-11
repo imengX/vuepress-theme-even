e4493dd00f261ee1c3cabfe0a7b67c00ab1ea81e
imengX <var.peony@gmail.com>
2018-06-12 00:29:47 +0800

    hello world

---
+++ layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{$title}}</h1>
+    <h2>{{$description}}</h2>
+    <div v-for="page in $site.pages">
+      <h2>{{page.title}}</h2>
+      <p>desc</p>
+    </div>
+    <Content/>    
+  </div>
+</template>
+ 
