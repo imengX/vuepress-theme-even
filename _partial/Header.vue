79bfee490e3546b033b76839b2885c454de8afe8
imengX <var.peony@gmail.com>
2018-06-15 01:14:15 +0800

    Header

---
+++ _partial/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="logo-wrapper">
+        <a :href="$localePath">{{ $siteTitle }}</a> 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus" scoped>
+.logo-wrapper {
+  float: left;
+
+  .logo {
+    font-size: $logo-font-size;
+    font-family: $logo-font-family;
+  }
+}
+</style>
--- layout.vue
+++ layout.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <Header></Header>
     <h1>{{$title}}</h1>
     <h2>{{$description}}</h2>
-    <div v-for="page in $site.pages">
+    <!-- <div v-if="$page.frontmatter.home" v-for="page in $site.pages">
       <h2>{{page.title}}</h2>
       <p>desc</p>
-    </div>
+    </div> -->
     <Content/>    
   </div>
 </template>
- 
+ <script>
+import Header from './_partial/Header.vue';
+
+export default {
+  components:{ Header },
+}
+</script>
